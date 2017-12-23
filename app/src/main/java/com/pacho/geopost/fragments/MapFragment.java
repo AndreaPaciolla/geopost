b6294c2402e7bbcda8ee518f478cfdb43a9d046c
Andrea Paciolla <andreapaciolla@gmail.com>
2017-12-23 20:11:03 +0100

    Fix onResume mapFragment

--- app/src/main/java/com/pacho/geopost/fragments/MapFragment.java
+++ app/src/main/java/com/pacho/geopost/fragments/MapFragment.java
@@ -10,6 +10,7 @@ import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -255,7 +256,9 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     @Override
     public void onResume() {
+        Log.d(TAG, "onResume: method called..");
         super.onResume();
+        mapView.onResume();
     }
 
     @Override
